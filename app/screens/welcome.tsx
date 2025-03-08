9141428b845f7833e508936c1945d021772a3d27
Nelson Rodriguez <neroxv1313@gmail.com>
2025-03-08 15:25:03 -0500

    commit

---
+++ app/screens/welcome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const welcome = () => {
+	return (
+		<View>
+			<Text> Welcome to Ikigai</Text>
+			<div> Welceom to Ikigai</div>
+		</View>
+	)
+}
+
+export default welcome;
