e99e993aa205eb6c90ea35236cfd75c80e5e157e
Nelson Rodriguez <neroxv1313@gmail.com>
2025-03-15 10:36:24 -0400

    Comitting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
 import { Stack } from "expo-router";
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <Stack />
+      </ApplicationProvider>
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View } from "react-native";
+import { Button, Layout, Text, Card } from '@ui-kitten/components';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Testing something right now</Text>
-    </View>
+    <Layout style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
+      <Card style={{ margin: 8 }}>
+        <Text category='h1'>UI Kitten Example</Text>
+        <Text category='s1' style={{ marginVertical: 16 }}>
+          UI Kitten is successfully installed and working!
+        </Text>
+        <Button>BUTTON</Button>
+      </Card>
+    </Layout>
   );
 }
